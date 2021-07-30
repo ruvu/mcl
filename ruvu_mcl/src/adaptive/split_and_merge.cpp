b32c810dd4fea743a34f12c00ea74b9511f1ee54
Ramon Wijnands <ramon@ruvu.nl>
2021-07-30 13:31:51 +0200

    feat: Switch to unordered_multimap for better performance

--- ruvu_mcl/src/adaptive/split_and_merge.cpp
+++ ruvu_mcl/src/adaptive/split_and_merge.cpp
@@ -26,31 +26,38 @@ void SplitAndMerge::merge_particles(ParticleFilter * pf) const
   // Based on paper: Monte Carlo localization for mobile robot using adaptive particle merging and splitting technique
 
   // Discretize particles:
-  std::unordered_map<Key, std::vector<Particle>, KeyHasher> grid_clusters;
+  std::unordered_multimap<Key, Particle, KeyHasher> grid_clusters;
   for (auto & particle : pf->particles) {
     int i = particle.pose.getOrigin().getX() / adaptive_config_.xy_grid_size;
     int j = particle.pose.getOrigin().getY() / adaptive_config_.xy_grid_size;
     int k = tf2::getYaw(particle.pose.getRotation()) /
             adaptive_config_.theta_grid_size;  // TODO(Paul): Combine -pi and +pi clusters
     auto key = std::make_tuple(i, j, k);
-    grid_clusters[key].push_back(std::move(particle));
+    grid_clusters.insert({key, particle});
   }
 
   // Construct new vector with merged particles:
   std::vector<Particle> merged_particles;
   size_t nr_particles = pf->particles.size();
-  for (const auto & cluster : grid_clusters) {
-    if (
-      cluster.second.size() == 1 ||
-      nr_particles < config_.min_particles + cluster.second.size() - 1) {
-      merged_particles.insert(merged_particles.end(), cluster.second.begin(), cluster.second.end());
+
+  // iterate through all elements in the multimap
+  // don't worry, a lot of elements are skipped
+  for (auto it = grid_clusters.begin(); it != grid_clusters.end();) {
+    // get the range of the current key
+    const auto range = grid_clusters.equal_range(it->first);
+    const auto cluster_size = std::distance(range.first, range.second);
+    if (cluster_size == 1 || nr_particles < config_.min_particles + cluster_size - 1) {
+      // copy all particles from this cluster
+      for (auto it = range.first; it != range.second; ++it) merged_particles.push_back(it->second);
     } else {
+      // merge all particles from this cluster
       double sum_x = 0;
       double sum_y = 0;
       double sum_cos_theta = 0;
       double sum_sin_theta = 0;
       double sum_weight = 0;
-      for (const auto & particle : cluster.second) {
+      for (auto it = range.first; it != range.second; ++it) {
+        const auto & particle = it->second;
         sum_x += particle.pose.getOrigin().getX();
         sum_y += particle.pose.getOrigin().getY();
         sum_cos_theta += cos(tf2::getYaw(particle.pose.getRotation()));
@@ -59,14 +66,17 @@ void SplitAndMerge::merge_particles(ParticleFilter * pf) const
       }
       tf2::Vector3 mean_origin;
       tf2::Quaternion mean_rotation;
-      mean_origin.setX(sum_x / cluster.second.size());
-      mean_origin.setY(sum_y / cluster.second.size());
+      mean_origin.setX(sum_x / cluster_size);
+      mean_origin.setY(sum_y / cluster_size);
       double mean_theta = atan2(sum_sin_theta, sum_cos_theta);
       mean_rotation.setRPY(0, 0, mean_theta);
       Particle mean_particle(tf2::Transform(mean_rotation, mean_origin), sum_weight);
       merged_particles.push_back(std::move(mean_particle));
-      nr_particles -= cluster.second.size() - 1;
+      nr_particles -= cluster_size - 1;
     }
+
+    // step to the end of the range
+    it = range.second;
   }
   ROS_DEBUG_NAMED(
     name, "merging reduced particles from %zu to %zu particles", pf->particles.size(),
